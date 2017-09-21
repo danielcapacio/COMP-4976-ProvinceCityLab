41d8c406e24462959c4716dae33636236a17021d
Daniel Capacio <danielcapacio@gmail.com>
2017-09-20 21:42:22 -0700

    City and Province models

---
+++ COMP4976ProvinceCityLab/Models/Province.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace COMP4976ProvinceCityLab.Models
+{
+    public class Province
+    {
+        [Key]
+        public string ProvinceCode { get; set; }
+        public string ProvinceName { get; set; }
+
+        public List<City> Cities { get; set; }
+    }
+}
