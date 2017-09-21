2f3e838b31720c516d3e54406c5b72f11fa6a2ff
Daniel Capacio <danielcapacio@gmail.com>
2017-09-20 21:48:51 -0700

    Created context using DbContext

---
+++ COMP4976ProvinceCityLab/Data/ClientContext.cs
@@ -0,0 +1,21 @@
+﻿using COMP4976ProvinceCityLab.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace COMP4976ProvinceCityLab.Data
+{
+    public class ClientContext : DbContext
+    {
+        public ClientContext() 
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Province> Provinces { get; set; }
+        public DbSet<City> Cities { get; set; }
+    }
+}
