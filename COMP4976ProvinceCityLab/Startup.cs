209896e89dc8b29ac9ef220f1cbf22eab1760c1e
Daniel Capacio <danielcapacio@gmail.com>
2017-09-20 21:36:04 -0700

    Add project files.

---
+++ COMP4976ProvinceCityLab/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP4976ProvinceCityLab.Startup))]
+namespace COMP4976ProvinceCityLab
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
